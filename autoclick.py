7684541ae493fcc750aefa7ca8872b8119cd2513
John William Jones III <johnwilliamjonesiii@gmail.com>
2022-08-09 13:06:35 -0500

    Installed Pygame for user GUI, added code from Star-Invasion to display GUI

--- autoclick.py
+++ autoclick.py
@@ -1,11 +1,244 @@
 # importing time and threading
+# Beginning of Pygame Code
+from pynput.keyboard import Listener, KeyCode
+from pynput.mouse import Button, Controller
+import os
+from os import path
+import sys
+import pygame
+import random
+from pygame.locals import *
 import time
 import threading
-from pynput.mouse import Button, Controller
 
+pygame.init()
+pygame.mixer.init()
+ROOT_DIR = os.getcwd()
+
+
+def run(runfile):
+    with open(runfile, "r") as rnf:
+        exec(rnf.read())
+
+
+##Game Settings##
+# Colours
+BACKGROUND_COLOR = (0, 0, 0)
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+YELLOW = (255, 255, 0)
+ORANGE = (130, 130, 0)
+LIME_GREEN = (0, 255, 0)
+FOREST_GREEN = (0, 150, 0)
+BLUE = (0, 0, 255)
+CYAN = (0, 255, 255)
+GREY = (123, 123, 123)
+RED = (255, 0, 0)
+# Game Border/Margin Settings
+WINDOW_WIDTH = 500
+WINDOW_HEIGHT = 600
+GAME_SIZE = WINDOW_HEIGHT * WINDOW_WIDTH
+GAME_SIDE_MARGIN = 20
+GAME_TOP_MARGIN = 20
+GAME_BOTTOM_MARGIN = 20
+GAME_BORDER_WIDTH = 3
+GAME_TOP_WALL = GAME_TOP_MARGIN + GAME_BORDER_WIDTH
+GAME_RIGHT_WALL = WINDOW_WIDTH - GAME_SIDE_MARGIN - GAME_BORDER_WIDTH
+GAME_BOTTOM_WALL = WINDOW_HEIGHT - GAME_BOTTOM_MARGIN - GAME_BORDER_WIDTH
+GAME_LEFT_WALL = GAME_SIDE_MARGIN + GAME_BORDER_WIDTH
+GAME_FPS = 40
+START_GAME = True
+ACTUAL_GAME_IS_BEING_PLAYED = True
+# Arrays for animated star background
+star_field_slow = []
+star_field_medium = []
+star_field_fast = []
+for slow_stars in range(50):
+    star_x_coordinate = random.randrange(0, WINDOW_WIDTH)
+    star_y_coordinate = random.randrange(0, WINDOW_HEIGHT)
+    star_field_slow.append([star_x_coordinate, star_y_coordinate])
+for medium_stars in range(35):
+    star_x_coordinate = random.randrange(0, WINDOW_WIDTH)
+    star_y_coordinate = random.randrange(0, WINDOW_HEIGHT)
+    star_field_medium.append([star_x_coordinate, star_y_coordinate])
+for fast_stars in range(15):
+    star_x_coordinate = random.randrange(0, WINDOW_WIDTH)
+    star_y_coordinate = random.randrange(0, WINDOW_HEIGHT)
+    star_field_fast.append([star_x_coordinate, star_y_coordinate])
+
+clock = pygame.time.Clock()
+game_display = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
+level_font = pygame.font.SysFont('Eurostile Bold Extended', 20, True)
+level_equals_font = pygame.font.SysFont('Eurostile Bold Extended', 10, True)
+score_font = pygame.font.SysFont('Eurostile Bold Extended', 10, True)
+score_equals_font = pygame.font.SysFont('Eurostile Bold Extended', 10, True)
+hero_lives_font = pygame.font.SysFont('Eurostile Bold Extended', 10, True)
+title_font = pygame.font.SysFont('Eurostile Bold Extended', 35, True)
+title_press_start_font = pygame.font.SysFont(
+    'Eurostile Bold Extended', 17, True)
+title_copyright_font = pygame.font.SysFont('Eurostile Bold Extended', 12, True)
+pygame.display.set_caption(' STAR INVASION ')
+
+
+def handle_events():
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            quit()
+# Main Menu
+# Text Renderer
+
+
+def text_format(message, textFont, textSize, textColor):
+    newFont = pygame.font.SysFont(textFont, textSize)
+    newText = newFont.render(message, 0, textColor)
+    return newText
+
+
+def main_menu_is_active():
+    show_background()
+    Show_Menu_Screen = True
+    Selected_Option = "PLAY"
+    while Show_Menu_Screen:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                quit()
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_UP and Selected_Option == "QUIT":
+                    Selected_Option = "CONTROLS"
+                elif event.key == pygame.K_UP and Selected_Option == "PLAY":
+                    Selected_Option = "QUIT"
+                elif event.key == pygame.K_UP and Selected_Option == "CONTROLS":
+                    Selected_Option = "PLAY"
+                elif event.key == pygame.K_DOWN and Selected_Option == "PLAY":
+                    Selected_Option = "CONTROLS"
+                elif event.key == pygame.K_DOWN and Selected_Option == "CONTROLS":
+                    Selected_Option = "QUIT"
+                elif event.key == pygame.K_DOWN and Selected_Option == "QUIT":
+                    Selected_Option = "PLAY"
+                if event.key == pygame.K_RETURN:
+                    if Selected_Option == "PLAY":
+                        Show_Menu_Screen = False
+                    if Selected_Option == "QUIT":
+                        pygame.quit()
+                        quit()
+
+            # Main Menu UI
+        game_display.blit(game_display, (0, 0))
+        game_display.fill(BACKGROUND_COLOR)
+        # title = text_format("   | STAR INVASION |", 'Eurostile Bold Extended', 70, WHITE)
+        if Selected_Option == "PLAY":
+            text_start = text_format(
+                "PLAY", 'Eurostile Bold Extended', 40, WHITE)
+        else:
+            text_start = text_format(
+                "PLAY", 'Eurostile Bold Extended', 40, GREY)
+        if Selected_Option == "CONTROLS":
+            text_controls = text_format(
+                "CONTROLS", 'Eurostile Bold Extended', 40, YELLOW)
+        else:
+            text_controls = text_format(
+                "CONTROLS", 'Eurostile Bold Extended', 40, ORANGE)
+        if Selected_Option == "QUIT":
+            text_quit = text_format(
+                "QUIT", 'Eurostile Bold Extended', 40, WHITE)
+        else:
+            text_quit = text_format(
+                "QUIT", 'Eurostile Bold Extended', 40, GREY)
+        for star in star_field_slow:
+            star[1] += 1
+            if star[1] > WINDOW_HEIGHT:
+                star[0] = random.randrange(0, WINDOW_WIDTH)
+                star[1] = random.randrange(-20, -5)
+            pygame.draw.circle(game_display, GREY, star, 3)
+        for star in star_field_medium:
+            star[1] += 4
+            if star[1] > WINDOW_HEIGHT:
+                star[0] = random.randrange(0, WINDOW_WIDTH)
+                star[1] = random.randrange(-20, -5)
+            pygame.draw.circle(game_display, GREY, star, 2)
+        for star in star_field_fast:
+            star[1] += 8
+            if star[1] > WINDOW_HEIGHT:
+                star[0] = random.randrange(0, WINDOW_WIDTH)
+                star[1] = random.randrange(-20, -5)
+            pygame.draw.circle(game_display, GREY, star, 1)
+
+        start_rect = text_start.get_rect()
+        quit_rect = text_quit.get_rect()
+        controls_rect = text_controls.get_rect()
+        game_display.blit(
+            text_start, (WINDOW_WIDTH/2 - (start_rect[2]/2), 300))
+        game_display.blit(text_quit, (WINDOW_WIDTH/2 - (quit_rect[2]/2), 420))
+        game_display.blit(text_controls, (WINDOW_WIDTH /
+                          2 - (controls_rect[2]/2), 360))
+        title_text = title_font.render('| STAR INVASION |', False, CYAN)
+        title_press_start_text = title_press_start_font.render(
+            'PRESS ENTER TO START', False, RED)
+        title_copyright_text = title_copyright_font.render(
+            '©2019 JOHN WILLIAM JONES III | TRUECODERS', False, BLUE)
+        pygame.display.flip()
+        clock.tick(GAME_FPS)
+
+
+def show_background():
+    game_display.blit(game_display, (0, 0))
+    game_display.fill(BACKGROUND_COLOR)
+    pygame.draw.rect(game_display, (BLACK), (GAME_SIDE_MARGIN, GAME_TOP_MARGIN,
+                     WINDOW_WIDTH - GAME_SIDE_MARGIN * 2, WINDOW_HEIGHT - GAME_BOTTOM_MARGIN * 2))
+    pygame.draw.rect(game_display, (BACKGROUND_COLOR), (GAME_LEFT_WALL, GAME_TOP_WALL, WINDOW_WIDTH - GAME_LEFT_WALL -
+                     GAME_SIDE_MARGIN - GAME_BORDER_WIDTH, WINDOW_HEIGHT - GAME_TOP_WALL - GAME_BOTTOM_MARGIN - GAME_BORDER_WIDTH))
+    for star in star_field_slow:
+        star[1] += 1
+        if star[1] > WINDOW_HEIGHT:
+            star[0] = random.randrange(0, WINDOW_WIDTH)
+            star[1] = random.randrange(-20, -5)
+        pygame.draw.circle(game_display, GREY, star, 3)
+    for star in star_field_medium:
+        star[1] += 4
+        if star[1] > WINDOW_HEIGHT:
+            star[0] = random.randrange(0, WINDOW_WIDTH)
+            star[1] = random.randrange(-20, -5)
+        pygame.draw.circle(game_display, CYAN, star, 2)
+    for star in star_field_fast:
+        star[1] += 8
+        if star[1] > WINDOW_HEIGHT:
+            star[0] = random.randrange(0, WINDOW_WIDTH)
+            star[1] = random.randrange(-20, -5)
+        pygame.draw.circle(game_display, YELLOW, star, 1)
+
+
+def pause_game():
+    game_is_paused = True
+    while game_is_paused:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                game_is_paused = False
+                ACTUAL_GAME_IS_BEING_PLAYED = False
+                quit()
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_p:
+                    game_is_paused = False
+
+        clock.tick(GAME_FPS)
+
+
+# Main Menu Is Open, Game Should Return To Main Menu After Death
+ACTUAL_GAME_IS_BEING_PLAYED = True
+while ACTUAL_GAME_IS_BEING_PLAYED == True:
+    main_menu_is_active()
+    while START_GAME:
+        handle_events()
+        # Display Background and Game Objects
+        show_background()
+        # Display score and UI
+        pygame.display.update()
+        clock.tick(GAME_FPS)
+
+# Beginning of Autoclicker Code
 # pynput.keyboard is used to watch events of
 # keyboard for start and stop of auto-clicker
-from pynput.keyboard import Listener, KeyCode
 
 
 # four variables are created to
